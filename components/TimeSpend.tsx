356908fb4358adc1ae637c89e59551ff4e601f9c
Carlos-Guanipa <carlosguanipa@kabalaw.com>
2023-04-20 18:47:46 -0400

    Changed time spend marker

--- components/TimeSpend.tsx
+++ components/TimeSpend.tsx
@@ -7,15 +7,16 @@ export default function TimeSpend() {
 	const { setCurrentCharCode, currentCharCode, gameState, answer, setAnswers } =
 		useContext(GameContext)
 
-	const [timeSpend, setTimeSpend] = useState<number>(0)
+	const [timeSpendInDs, setTimeSpendInDs] = useState<number>(0)
+	const [timeSpend, setTimeSpend] = useState<string>("")
 
 	useEffect(() => {
 		const charInterval = setInterval(() => {
-			setTimeSpend((old) => old + 100)
+			setTimeSpendInDs((old) => old + 1)
 		}, 100)
 		if (gameState === "END" || gameState === "IDLE") {
 			clearTimeout(charInterval)
-			setTimeSpend(0)
+			setTimeSpendInDs(0)
 		}
 		return () => {
 			clearTimeout(charInterval)
@@ -23,40 +24,60 @@ export default function TimeSpend() {
 	}, [gameState])
 
 	useEffect(() => {
-		if (timeSpend >= 6000) {
+		if (timeSpendInDs >= 900) {
 			setAnswers((old) =>
 				[
 					{
 						charCode: currentCharCode,
-						timeTaken: timeSpend,
+						timeTaken: timeSpendInDs,
 						word: "Sin respuesta",
 					},
 				].concat(old)
 			)
-			setTimeSpend(0)
+			setTimeSpendInDs(0)
 			setCurrentCharCode((old) => old + 1)
+			return
 		}
-	}, [timeSpend])
+	}, [timeSpendInDs])
 
 	useEffect(() => {
+		const decisecondPassed = timeSpendInDs % 10 === 0 ? 0 : timeSpendInDs % 10
+		const secondsPassed = timeSpendInDs >= 600 ? Math.floor((timeSpendInDs % 600)/10) : Math.floor(timeSpendInDs / 10) 
+		const minutesPassed = Math.floor(timeSpendInDs / 600)
+		const secondsPassedString =
+			secondsPassed < 10 ? `0${secondsPassed}` : `${secondsPassed}`
+		const minutesPassedString =
+			minutesPassed < 10 ? `0${minutesPassed}` : `${minutesPassed}`
+		const dsPassedString =
+			decisecondPassed < 10 ? `0${decisecondPassed}` : `${decisecondPassed}`
+		setTimeSpend(`${minutesPassedString}:${secondsPassedString}:${dsPassedString}`)
+		
+	}, [timeSpendInDs])
+
+	useEffect(() => {
+		const timeSpendInSeconds = timeSpendInDs / 10
 		if (answer.length) {
 			setAnswers((old) =>
 				[
 					{
 						charCode: currentCharCode,
-						timeTaken: timeSpend,
+						timeTaken: timeSpendInSeconds,
 						word: answer,
 					},
 				].concat(old)
 			)
-			setTimeSpend(0)
+			setTimeSpendInDs(0)
 		}
 	}, [answer])
 
 	return (
-		<span className='w-[20ch] text-center p-3 flex flex-col justify-center items-center'>
-			Tiempo transcurrido:{" "}
-			<span className='block w-[4ch] self-center'>{timeSpend}</span> segundos
-		</span>
+		<>
+			<span className='w-[20ch] text-center'>
+				Tiempo transcurrido
+			</span>
+			<span className='w-[8ch] text-center'>
+				{timeSpend}
+			</span>
+		</>
 	)
 }
