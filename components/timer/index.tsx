c299c9de91187ed0c6da95e29f9c12367ef4c9da
NewCastile <carlosaguanipa1@gmail.com>
2024-01-16 17:02:35 -0400

    fixed component size changes on every render

--- components/timer/index.tsx
+++ components/timer/index.tsx
@@ -6,7 +6,15 @@ const Timer = () => {
   return (
     <>
       <span className={"w-[20ch] text-center"}>Time passed</span>
-      <span className={"w-[8ch] text-center"}>{timeSpend}</span>
+      <span className={"flex w-[8ch] flex-row items-center justify-center space-x-1"}>
+        {Array.from(timeSpend).map((character, characterIdx) => {
+          return (
+            <span key={characterIdx} className={"inline-block w-3 text-center"}>
+              {character}
+            </span>
+          );
+        })}
+      </span>
     </>
   );
 };
