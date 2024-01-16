b9741e36ad6f1b61b12f7d16e323bd17c1cdca28
NewCastile <carlosaguanipa1@gmail.com>
2024-01-16 13:56:05 -0400

    update for theme configuration extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
-/** @format */
+/** @type {import('tailwindcss').Config} */
 
 module.exports = {
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        neu: {
+          blue: "#0091ff",
+          red: "#ff0000",
+          white: "#dedede",
+          gray: "#c4c4c4",
+          salmon: "#f75a5a",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
